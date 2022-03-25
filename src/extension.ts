b3dbe23f096b01efe1eade8b67184d852618fbf5
Alessandro Sangalli <alessandrosangalli@vipcommerce.com.br>
2022-03-25 02:37:38 -0300

    feat: add mob not found warning

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { MobViewProvider } from "./views/mob-view-provider";
+const cp = require("child_process");
 
 vscode.window.createTreeView("mobUtils", {
   treeDataProvider: new MobViewProvider(),
@@ -11,6 +12,18 @@ export function activate(context: vscode.ExtensionContext) {
   const terminal = vscode.window.createTerminal(`Ext Mob GUI terminal}`);
 
   let commands = [
+    vscode.commands.registerCommand("mob-vscode-gui.mobCommandExists", () => {
+      cp.exec(
+        'command -v mob >/dev/null && echo "found" || echo "not found"',
+        (err: string, stdout: string, stderr: string) => {
+          if (stdout !== "found") {
+            vscode.window.showErrorMessage(
+              "Mob command not found. Please install Mob CLI: https://mob.sh"
+            );
+          }
+        }
+      );
+    }),
     vscode.commands.registerCommand("mob-vscode-gui.start", () => {
       terminal.sendText("mob start");
     }),
@@ -25,6 +38,8 @@ export function activate(context: vscode.ExtensionContext) {
   for (const command of commands) {
     context.subscriptions.push(command);
   }
+
+  vscode.commands.executeCommand("mob-vscode-gui.mobCommandExists");
 }
 
 // this method is called when your extension is deactivated
